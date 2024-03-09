9e250996ab2b8cdf24972f7e0a99e255629a0f68
Arivola Ursulla <ursullaarivola@gmail.com>
2024-03-08 22:06:11 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-first-app';
+  name: string = "Marie";
+  grocery = 
+    [{
+      name: 'Apple',
+      quantity: 3,
+      id: 1,
+    },
+    {
+      name: 'Oranges',
+      quantity: 2,
+      id: 2,
+    },
+    {
+      name: 'Cucumber',
+      quantity: 4,
+      id: 3,
+    },
+    {
+      name: 'Potatos',
+      quantity: 4,
+      id: 4,
+    }];
+  getName(){
+    return this.name;
+  }
 }
